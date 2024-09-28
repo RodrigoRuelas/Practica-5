b662944900a7dde6fc00f4fc8c3fe1b6f1a38997
Victus <rruelasl@ulasalle.edu.pe>
2024-09-28 00:58:03 -0500

    Reproduciendo un audio con el objeto MediaPlayer

--- app/src/main/java/com/example/reproductormusica/MainActivity.kt
+++ app/src/main/java/com/example/reproductormusica/MainActivity.kt
@@ -1,5 +1,11 @@
+// Proyecto: Reproductor de musica
+// De: Rodrigo Alonso Ruelas Lope
+// Fecha de Creacion: 28/09/2024 - 12:50 am
+// Ultima modificacion: 28/09/2024 -  am
+
 package com.example.reproductormusica
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,14 +13,29 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    // Creando fichero de audio
+    val fd by lazy {
+        assets.openFd("Fragil - Avenida Larco.mp3")
+    }
+
+    // Objero MediaPlayer
+    val mp by lazy {
+        val m = MediaPlayer()
+        m.setDataSource(
+            fd.fileDescriptor,
+            fd.startOffset,
+            fd.length
+        )
+        fd.close()
+        m.prepare()
+        m
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        mp.start()
     }
 }
